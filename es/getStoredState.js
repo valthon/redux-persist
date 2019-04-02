dc2e065e8cfcfc472a3a891b7dfbbba49a65a46b
David J Parrott <valthon@nothlav.net>
2019-04-01 21:57:02 -0400

    prebuild

---
+++ es/getStoredState.js
@@ -0,0 +1,41 @@
+
+
+import { KEY_PREFIX } from './constants';
+
+export default function getStoredState(config) {
+  var transforms = config.transforms || [];
+  var storageKey = '' + (config.keyPrefix !== undefined ? config.keyPrefix : KEY_PREFIX) + config.key;
+  var storage = config.storage;
+  var debug = config.debug;
+  var deserialize = void 0;
+  if (!config.deserialize) {
+    deserialize = defaultDeserialize;
+  } else if (typeof config.deserialize === 'function') {
+    deserialize = config.deserialize;
+  } else {
+    deserialize = function deserialize(x) {
+      return x;
+    };
+  }
+  return storage.getItem(storageKey).then(function (serialized) {
+    if (!serialized) return undefined;else {
+      try {
+        var state = {};
+        var rawState = deserialize(serialized);
+        Object.keys(rawState).forEach(function (key) {
+          state[key] = transforms.reduceRight(function (subState, transformer) {
+            return transformer.out(subState, key, rawState);
+          }, deserialize(rawState[key]));
+        });
+        return state;
+      } catch (err) {
+        if (process.env.NODE_ENV !== 'production' && debug) console.log('redux-persist/getStoredState: Error restoring data ' + serialized, err);
+        throw err;
+      }
+    }
+  });
+}
+
+function defaultDeserialize(serial) {
+  return JSON.parse(serial);
+}
